8c390ee0d41032242210045cab26078f9fc46b65
r3m4g <astitvanikose.an@gmail.com>
2020-04-12 20:14:50 +0530

    Updated

--- covid19.py
+++ covid19.py
@@ -7,7 +7,7 @@ page = urllib2.urlopen(quote_page)
 
 soup = BeautifulSoup(page, 'html.parser')
 
-name_box = soup.find('div', attrs={'class': 'information_block'})
+name_box = soup.find('div', attrs={'class': 'site-stats-count'})
 
 name = name_box.text.strip()
 print(bold(red(name)))
